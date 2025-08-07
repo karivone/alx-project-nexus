88fa87de3e86b59c128039d15e47048c3d2c7710
Karimi Yvonne <55494837+karivone@users.noreply.github.com>
2025-08-07 14:31:07 +0300

    movie recommendation

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to the Movie Recommendation API!")
+#def home(request):
+ #   return HttpResponse("Welcome to the Movie Recommendation API!")
+
+import os
+import requests
+from django.shortcuts import render
+
+def movies(request):
+    TMDB_API_KEY = os.getenv('TMDB_API_KEY', 'dabc96b5a972e54425d4efd3010e893d')
+    url = f'https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=en-US&page=1'
+    response = requests.get(url)
+    movies = response.json().get('results', [])
+    return render(request, 'movies.html', {'movies': movies})
